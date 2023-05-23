fcfef15f28e45d66685bad2a32c494d8a0283eb1
Rajendro1 <rajandroprosad@gmail.com>
2023-05-23 07:02:33 +0530

    test

--- AccuKnoxApi/main.go
+++ AccuKnoxApi/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	pgdatabase "github.com/Rajendro1/Projects/AccuKnoxApi/pgDatabase"
-	
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	pgdatabase.Connect(&gin.Context{})
-	// routers.HandleRequest()
+	routers.HandleRequest()
 }
